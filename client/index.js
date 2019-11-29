d2eea7b5ac01a7419223f688e1db644a01443bcb
Justin Lim <nockmss@gmail.com>
2019-11-29 21:40:14 +0000

    Client: Use image as buttons instead

--- client/index.js
+++ client/index.js
@@ -1,49 +1,74 @@
+//
+// Configuration.
+//
 const ADDRESS = 'localhost:80/mobcontrol/';
-const UPDATES_PER_SECOND = 1; // Set to 60 at some point.
+const UPDATES_PER_SECOND = 60; // Set to 60 at some point.
 
-const buttonContainer = document.getElementById('buttonContainer');
-const socket = new WebSocket(`ws://${ADDRESS}`);
+//
+// Load the canvas, context.
+//
+const hitboxCanvas = document.getElementById('hitboxCanvas');
+const hitboxContext = hitboxCanvas.getContext('2d');
 
-const buttonNames = [
-    'BUTTON_A',
-    'BUTTON_B',
-    'BUTTON_X',
-    'BUTTON_Y',
-    'BUTTON_BACK',
-    'BUTTON_GUIDE',
-    'BUTTON_START',
-    'BUTTON_LEFTSTICK',
-    'BUTTON_RIGHTSTICK',
-    'BUTTON_LEFTSHOULDER',
-    'BUTTON_RIGHTSHOULDER',
-    'BUTTON_DPAD_UP',
-    'BUTTON_DPAD_DOWN',
-    'BUTTON_DPAD_LEFT',
-    'BUTTON_DPAD_RIGHT',
-];
+//
+// Load the image.
+//
+const hitboxImage = new Image;
+hitboxImage.addEventListener('load', function() {
+    hitboxContext.drawImage(hitboxImage, 0, 0, hitboxCanvas.width, hitboxCanvas.height);
+}, false);
+hitboxImage.src = './layout_logical.png';
 
+//
+// Define the buttons.
+//
+const buttonColours = {
+    '255,255,0,255': 'BUTTON_A',
+    '255,255,1,255': 'BUTTON_B',
+    '255,255,2,255': 'BUTTON_X',
+    '255,255,3,255': 'BUTTON_Y',
+    '255,255,4,255': 'BUTTON_BACK',
+    '255,255,5,255': 'BUTTON_GUIDE',
+    '255,255,6,255': 'BUTTON_START',
+    '255,255,7,255': 'BUTTON_LEFTSTICK',
+    '255,255,8,255': 'BUTTON_RIGHTSTICK',
+    '255,255,9,255': 'BUTTON_LEFTSHOULDER',
+    '255,255,0,255': 'BUTTON_RIGHTSHOULDER',
+    '255,255,11,255': 'BUTTON_DPAD_UP',
+    '255,255,12,255': 'BUTTON_DPAD_DOWN',
+    '255,255,13,255': 'BUTTON_DPAD_LEFT',
+    '255,255,14,255': 'BUTTON_DPAD_RIGHT',
+};
+
+//
+// Initialise state.
+//
+const buttonIds = Object.values(buttonColours);
 const buttonState = {};
+buttonIds.forEach(buttonId => { buttonState[buttonId] = false; })
 
-function getButton(id) {
-    const element = document.createElement('div');
-    element.innerText = id;
-    element.ontouchstart = () => { buttonState[id] = true; console.log(buttonState) };
-    element.ontouchend = () => { buttonState[id] = false; console.log(buttonState) };
-    element.onmousedown = () => { buttonState[id] = true; console.log(buttonState) };
-    element.onmouseup = () => { buttonState[id] = false; console.log(buttonState) };
-
-    return {
-        id,
-        element,
-    };
+//
+// Update state when button press and release.
+//
+function onCanvasInteraction(ev, activate) {
+    const imageX = (ev.clientX / hitboxCanvas.scrollWidth) * hitboxCanvas.width;
+    const imageY = (ev.clientY / hitboxCanvas.scrollHeight) * hitboxCanvas.height;
+    var imagePixel = hitboxContext.getImageData(imageX, imageY, 1, 1).data;
+    var imagePixelString = imagePixel.join(',');
+    var buttonId = buttonColours[imagePixelString];
+    if (buttonId) { buttonState[buttonId] = activate; }
+    console.log(buttonId);
 }
 
-const buttons = buttonNames.map(getButton);
+hitboxCanvas.onmousedown = (ev) => { onCanvasInteraction(ev, true); }
+hitboxCanvas.ontouchend = (ev) => { onCanvasInteraction(ev, false); }
+hitboxCanvas.onmousedown = (ev) => { onCanvasInteraction(ev, true); }
+hitboxCanvas.onmouseup = (ev) => { onCanvasInteraction(ev, false); }
 
-buttons.forEach(button => {
-    buttonContainer.appendChild(button.element);
-})
-
+//
+// Transmit padState on regular intervals.
+//
+const socket = new WebSocket(`ws://${ADDRESS}`);
 setInterval(() => {
     socket.send(JSON.stringify(buttonState, null, 4));
 }, 1000 / UPDATES_PER_SECOND);
