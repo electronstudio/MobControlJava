1f9f04fdd43b11568e5ac61906fa9e66a676a58a
Justin Lim <nockmss@gmail.com>
2019-12-05 19:14:16 +0000

    Support ESLInt@VSC

---
+++ src/main/resources/client/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es6": true
+	},
+	"plugins": [
+		"html"
+	],
+	"extends": "airbnb",
+	"rules": {
+		"linebreak-style": [2, "windows"],
+		"indent": ["error", "tab"],
+		"no-tabs": 0
+	}
+};
