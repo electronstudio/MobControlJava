ba0413a34a5ba551776d2aeb6cd51bc2e11edc05
Justin <nockmss@gmail.com>
2019-12-08 21:45:09 +0000

    Show settings page in portrait

--- src/main/resources/client/index.js
+++ src/main/resources/client/index.js
@@ -9,9 +9,25 @@ import PadPage from './pages/pad.js';
 import Conn from './lib/conn.js';
 import Logger from './lib/logger.js';
 
+const pageContainer = document.getElementById('pageContainer');
+const padPageElement = document.getElementById('padPage');
+const setPageElement = document.getElementById('setPage');
 const logElement = document.getElementById('log');
 const logger = new Logger(logElement);
 
 const conn = new Conn();
 const padPage = new PadPage(conn, logger);
 export default padPage;
+
+function showElement(element, show) {
+	element.style.opacity = show ? '1' : '0';
+	element.style.pointerEvents = show ? 'auto' : 'none';
+}
+
+window.addEventListener('resize', (ev) => {
+	const width = pageContainer.scrollWidth;
+	const height = pageContainer.scrollHeight;
+	const portrait = width < height;
+	showElement(setPageElement, portrait);
+	showElement(padPageElement, !portrait);
+});
